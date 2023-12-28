9aea236aa48c3ac0ee022a7281937cdd74a6c467
lunarmoon7 <49crehbgr@gmail.com>
2023-12-28 11:40:43 +0900

    init: 프로젝트 생성 및 초기 설정

--- src/utils/api_paths.ts
+++ src/utils/api_paths.ts
@@ -1,58 +1,56 @@
-/* Base Url */
-const BASE_URL = 'https://kdt.frontend.5th.programmers.co.kr'
 /* 어드민 */
-const CREATE_CHANNEL_BY_ADMIN_PATH = '/channels/create'
+export const CREATE_CHANNEL_BY_ADMIN_PATH = '/channels/create'
 
 /* 인증 */
-const LOGIN_PATH = '/login'
-const SIGNUP_PATH = '/signup'
-const LOGOUT_PATH = '/logout'
+export const LOGIN_PATH = '/login'
+export const SIGNUP_PATH = '/signup'
+export const LOGOUT_PATH = '/logout'
 
 /* 사용자 */
-const GET_ALL_USER_LIST_PATH = '/users/get-users'
-const GET_USER_PROFILE_PATH = '/users'
-const UPDATE_PROFILE_IMAGE_PATH = '/users/upload-photo'
-const UPDATE_COVER_IMAGE_PATH = '/users/upload-photo'
+export const GET_ALL_USER_LIST_PATH = '/users/get-users'
+export const GET_USER_PROFILE_PATH = '/users'
+export const UPDATE_PROFILE_IMAGE_PATH = '/users/upload-photo'
+export const UPDATE_COVER_IMAGE_PATH = '/users/upload-photo'
 
 /* 설정 */
-const UPDATE_MY_PROFILE_PATH = '/settings/update-user'
-const UPDATE_MY_PASSWORD_PATH = '/settings/update-password'
+export const UPDATE_MY_PROFILE_PATH = '/settings/update-user'
+export const UPDATE_MY_PASSWORD_PATH = '/settings/update-password'
 
 /* 채널 */
-const GET_CHANNEL_LIST_PATH = '/channels'
-const GET_CHANNEL_DETAIL_PATH = '/channel'
+export const GET_CHANNEL_LIST_PATH = '/channels'
+export const GET_CHANNEL_DETAIL_PATH = '/channel'
 
 /* 포스트 */
-const GET_POST_LIST_BY_CHANNEL_PATH = '/posts/channel'
-const GET_POST_LIST_BY_USER_PATH = '/posts/author'
-const CREATE_POST_PATH = '/posts/create'
-const GET_DETAIL_POST_PATH = '/posts'
-const UPDATE_POST_PATH = '/posts/update'
-const DELETE_POST_PATH = '/posts/delete'
+export const GET_POST_LIST_BY_CHANNEL_PATH = '/posts/channel'
+export const GET_POST_LIST_BY_USER_PATH = '/posts/author'
+export const CREATE_POST_PATH = '/posts/create'
+export const GET_DETAIL_POST_PATH = '/posts'
+export const UPDATE_POST_PATH = '/posts/update'
+export const DELETE_POST_PATH = '/posts/delete'
 
 /* 좋아요 */
-const LIKE_POST_PATH = '/likes/create'
-const UNLIKE_POST_PATH = '/likes/delete'
+export const LIKE_POST_PATH = '/likes/create'
+export const UNLIKE_POST_PATH = '/likes/delete'
 
 /* 댓글 */
-const CREATE_COMMENT_PATH = '/comments/create'
-const DELETE_COMMENT_PATH = '/comments/delete'
+export const CREATE_COMMENT_PATH = '/comments/create'
+export const DELETE_COMMENT_PATH = '/comments/delete'
 
 /* 알림 */
-const GET_NOTIFICATIONS_PATH = '/notifications'
-const UPDATE_NOTIFICATIONS_PATH = '/notifications/seen'
-const CREATE_NOTIFICATIONS_PATH = '/notifications/create'
+export const GET_NOTIFICATIONS_PATH = '/notifications'
+export const UPDATE_NOTIFICATIONS_PATH = '/notifications/seen'
+export const CREATE_NOTIFICATIONS_PATH = '/notifications/create'
 
 /* 팔로우 */
-const FOLLOW_PATH = '/follow/create'
-const UNFOLLOW_PATH = '/follow/delete'
+export const FOLLOW_PATH = '/follow/create'
+export const UNFOLLOW_PATH = '/follow/delete'
 
 /* 메시지 */
-const GET_USER_MESSAGE_PATH = '/messages/conversations'
-const GET_MESSAGE_LIST_BY_USER_PATH = '/messages'
-const CREATE_MESSAGE_PATH = '/messages/create'
-const UPDATE_MESSAGE_PATH = '/messages/update-seen'
+export const GET_USER_MESSAGE_PATH = '/messages/conversations'
+export const GET_MESSAGE_LIST_BY_USER_PATH = '/messages'
+export const CREATE_MESSAGE_PATH = '/messages/create'
+export const UPDATE_MESSAGE_PATH = '/messages/update-seen'
 
 /* 검색 */
-const SEARCH_USERS_PATH = '/search/users'
-const SEARCH_ALL_PATH = '/search/all'
+export const SEARCH_USERS_PATH = '/search/users'
+export const SEARCH_ALL_PATH = '/search/all'
