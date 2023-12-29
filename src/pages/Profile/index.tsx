4b0d1a0df41367097084d8eca669f0fa6c926902
lunarmoon7 <49crehbgr@gmail.com>
2023-12-29 13:45:05 +0900

    style: 사용자 이미지 컴포넌트를 추가했습니다.
    - `CoverImage`: 사용자 커버 이미지 컴포넌트입니다.
    - `DetailSection`: 사용자 프로필 이미지와 편집 버튼을 포함하는 컴포넌트입니다.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -10,6 +10,31 @@ const Header = styled.header`
   ${tw`w-full h-16 absolute top-0 left-0`}
 `
 
+const UserProfileSection = styled.section`
+  ${tw`w-full h-full`}
+`
+
+const CoverImage = styled.img`
+  ${tw`w-full h-64 object-cover`}
+`
+
+const UserProfileImage = styled.img`
+  ${tw`w-32 h-32 rounded-full object-cover z-10 bg-white`}
+`
+
+const DetailSection = styled.div`
+  ${tw`mx-auto w-10/12 flex justify-between items-center relative -mt-16`}
+`
+
+const Divider = styled.div`
+  ${tw`mx-auto w-10/12 h-[.3px] bg-[#000] my-6`}
+`
+
+const EditButton = styled.button`
+  background: url('src/assets/icons/edit.svg') no-repeat center center;
+  ${tw`w-6 h-6 self-end mb-3`}
+`
+
 function Profile() {
   const [userInfo, setUserInfo] = useState<User | null>(null)
 
@@ -31,6 +56,18 @@ function Profile() {
           />
         </button>
       </Header>
+      <UserProfileSection>
+        <CoverImage src={`${userInfo?.coverImage}`} />
+        <DetailSection>
+          <aside className="w-full flex flex-col gap-2">
+            <div className="w-full flex justify-between">
+              <UserProfileImage src={`${userInfo?.image}`} />
+              <EditButton />
+            </div>
+          </aside>
+        </DetailSection>
+        <Divider />
+      </UserProfileSection>
     </ProfileContainer>
   )
 }
