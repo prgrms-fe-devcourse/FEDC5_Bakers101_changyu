b41389864080cc6857a6592d37f956ede2d56ebe
lunarmoon7 <49crehbgr@gmail.com>
2023-12-30 18:46:58 +0900

    feat: 프로필, 커버 이미지 변경 API를 호출합니다.

---
+++ src/apis/uploadImage.ts
@@ -0,0 +1,22 @@
+import { UPDATE_PROFILE_IMAGE_PATH } from '@/utils/api_paths'
+import axiosInstance from './api'
+
+async function uploadImage(formData: FormData) {
+  try {
+    const response = await axiosInstance.post(
+      UPDATE_PROFILE_IMAGE_PATH,
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    )
+    return response.data
+  } catch (error) {
+    throw new Error('Error while uploading image')
+  }
+}
+
+export default uploadImage
