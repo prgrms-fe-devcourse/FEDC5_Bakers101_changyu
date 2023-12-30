f081aba719344c5e1f9b0c5d15e41a21ad6ca3c4
lunarmoon7 <49crehbgr@gmail.com>
2023-12-30 18:51:20 +0900

    feat: FormData 형식의 이미지를 추가하는 함수를 추가했습니다.

---
+++ src/utils/handleImageFormData.ts
@@ -0,0 +1,21 @@
+interface ImageFormData {
+  imageFile: File
+  type: 'Profile' | 'Post'
+  isCover?: boolean
+}
+
+function handleImageFormData({
+  imageFile,
+  type,
+  isCover
+}: ImageFormData): FormData {
+  const formData = new FormData()
+
+  formData.append('image', imageFile)
+
+  if (type === 'Profile') formData.append('isCover', String(isCover))
+
+  return formData
+}
+
+export default handleImageFormData
