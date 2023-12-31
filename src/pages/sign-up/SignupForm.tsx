6a12ed86a8f11ee46b8c0f58d8a1b4c91c6d62a9
lja0395 <kittyja1023@naver.com>
2023-12-31 19:22:43 +0900

    style: 회원가입 스타일 추가, 유효성검사 수정

--- src/pages/sign-up/SignupForm.tsx
+++ src/pages/sign-up/SignupForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { signUp } from '@/apis/signup';
+import * as styles from './SignupStyles';
 
 type Props = {};
 
@@ -8,96 +9,110 @@ function SignUp({}: Props) {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
   const validateEmail = (input: string): boolean => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(input);
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const isValid = emailRegex.test(input);
+    setEmailError(isValid ? '' : '올바른 이메일 주소를 입력하세요.');
+    return isValid;
   };
 
   const validatePassword = (input: string): boolean => {
-    return input.length >= 6;
+    const isValid = input.length >= 6;
+    setPasswordError(isValid ? '' : '비밀번호는 최소 6자리 이상이어야 합니다.');
+    return isValid;
   };
 
   const validateName = (input: string): boolean => {
-    return input.length >= 2;
+    const isValid = input.length >= 2;
+    setNameError(isValid ? '' : '이름은 최소 2글자 이상이어야 합니다.');
+    return isValid;
+  };
+
+  const validateConfirmPassword = (input: string): boolean => {
+    const isValid = input === password;
+    setConfirmPasswordError(isValid ? '' : '비밀번호와 확인 비밀번호가 일치하지 않습니다.');
+    return isValid;
   };
 
   const handleSumbit = async () => {
     try {
-      if (!validateEmail(email)) {
-        setError('올바른 이메일 주소를 입력하세요.');
-        return;
-      }
+      const isEmailValid = validateEmail(email);
+      const isNameValid = validateName(name);
+      const isPasswordValid = validatePassword(password);
+      const isConfirmPasswordValid = validateConfirmPassword(confirmPassword);
 
-      if (!validateName(name)) {
-        setError('이름은 최소 2글자 이상이어야 합니다.');
-        return;
+      if (isEmailValid && isNameValid && isPasswordValid && isConfirmPasswordValid) {
+        const response = await signUp(email, name, password);
+        console.log('response:', response);
       }
-
-      if (!validatePassword(password)) {
-        setError('비밀번호는 최소 6자리 이상이어야 합니다.');
-        return;
-      }
-
-      if (password !== confirmPassword) {
-        setError('비밀번호와 확인 비밀번호가 일치하지 않습니다.');
-        return;
-      }
-
-      // 유효성 검사 통과 시 회원가입 시도
-      const response = await signUp(email, name, password);
-      console.log('response:', response);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div>
-      <input
-        type="email"
-        className="border"
-        placeholder="이메일 주소"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-          setError('');
-        }}
-      />
-      <input
-        type="text"
-        className="border"
-        placeholder="이름"
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-          setError('');
-        }}
-      />
-      <input
-        type="password"
-        className="border"
-        placeholder="비밀번호"
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-          setError('');
-        }}
-      />
-      <input
-        type="password"
-        className="border"
-        placeholder="비밀번호 확인"
-        value={confirmPassword}
-        onChange={(e) => {
-          setConfirmPassword(e.target.value);
-          setError('');
-        }}
-      />
-      <button onClick={handleSumbit}>회원가입</button>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-    </div>
+    <styles.container>
+      <styles.form>
+        <styles.inputContainer>
+          <styles.Label>Email</styles.Label>
+          <styles.input
+            type="email"
+            placeholder="이메일 주소"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              validateEmail(e.target.value);
+            }}
+          />
+          {emailError && <styles.error>{emailError}</styles.error>}
+        </styles.inputContainer>
+        <styles.inputContainer>
+          <styles.Label>Name</styles.Label>
+          <styles.input
+            type="text"
+            placeholder="이름"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+              validateName(e.target.value);
+            }}
+          />
+          {nameError && <styles.error>{nameError}</styles.error>}
+        </styles.inputContainer>
+        <styles.inputContainer>
+          <styles.Label>Password</styles.Label>
+          <styles.input
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              validatePassword(e.target.value);
+            }}
+          />
+          {passwordError && <styles.error>{passwordError}</styles.error>}
+        </styles.inputContainer>
+        <styles.inputContainer>
+          <styles.Label>Confirm Password</styles.Label>
+          <styles.input
+            type="password"
+            placeholder="비밀번호 확인"
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+              validateConfirmPassword(e.target.value);
+            }}
+          />
+          {confirmPasswordError && <styles.error>{confirmPasswordError}</styles.error>}
+        </styles.inputContainer>
+        <styles.button onClick={handleSumbit}>JOIN</styles.button>
+      </styles.form>
+    </styles.container>
   );
 }
 
