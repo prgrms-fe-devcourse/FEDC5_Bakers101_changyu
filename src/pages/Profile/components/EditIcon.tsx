b95b2fd8dc9f76315a449417d46e42b1e72f1a3b
lunarmoon7 <49crehbgr@gmail.com>
2023-12-31 16:17:35 +0900

    style: 편집 아이콘 컴포넌트 추가

---
+++ src/pages/Profile/components/EditIcon.tsx
@@ -0,0 +1,22 @@
+// icon:edit | Feathericons https://feathericons.com/ | Cole Bemis
+import * as React from 'react'
+
+function EditIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      {...props}>
+      <path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7" />
+      <path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z" />
+    </svg>
+  )
+}
+
+export default EditIcon
