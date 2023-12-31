d0111cd72f85078eff7cb9111cbe24812d96b68d
lja0395 <kittyja1023@naver.com>
2023-12-31 17:48:07 +0900

    feat: 회원가입 유효성 검사 추가

--- src/pages/sign-up/SignupForm.tsx
+++ src/pages/sign-up/SignupForm.tsx
@@ -8,9 +8,44 @@ function SignUp({}: Props) {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const validateEmail = (input: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(input);
+  };
+
+  const validatePassword = (input: string): boolean => {
+    return input.length >= 6;
+  };
+
+  const validateName = (input: string): boolean => {
+    return input.length >= 2;
+  };
 
   const handleSumbit = async () => {
     try {
+      if (!validateEmail(email)) {
+        setError('올바른 이메일 주소를 입력하세요.');
+        return;
+      }
+
+      if (!validateName(name)) {
+        setError('이름은 최소 2글자 이상이어야 합니다.');
+        return;
+      }
+
+      if (!validatePassword(password)) {
+        setError('비밀번호는 최소 6자리 이상이어야 합니다.');
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        setError('비밀번호와 확인 비밀번호가 일치하지 않습니다.');
+        return;
+      }
+
+      // 유효성 검사 통과 시 회원가입 시도
       const response = await signUp(email, name, password);
       console.log('response:', response);
     } catch (error) {
@@ -25,30 +60,43 @@ function SignUp({}: Props) {
         className="border"
         placeholder="이메일 주소"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => {
+          setEmail(e.target.value);
+          setError('');
+        }}
       />
       <input
         type="text"
         className="border"
         placeholder="이름"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => {
+          setName(e.target.value);
+          setError('');
+        }}
       />
       <input
         type="password"
         className="border"
         placeholder="비밀번호"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(e) => {
+          setPassword(e.target.value);
+          setError('');
+        }}
       />
       <input
         type="password"
         className="border"
         placeholder="비밀번호 확인"
         value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
+        onChange={(e) => {
+          setConfirmPassword(e.target.value);
+          setError('');
+        }}
       />
       <button onClick={handleSumbit}>회원가입</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
 }
