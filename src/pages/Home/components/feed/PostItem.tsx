38ef43b7bebe7ab5e70b18b6f5e81d1898cbf9d5
JangGwon <tlsakch510@naver.com>
2024-01-01 00:10:30 +0900

    feat : 메인 홈 포스트리스트 - 포스트 아이템 올린 시간 1시간 이내이면 분단위로 표시

--- src/pages/Home/components/feed/PostItem.tsx
+++ src/pages/Home/components/feed/PostItem.tsx
@@ -20,26 +20,32 @@ type PostItemType = {
 const PostlItem = ({postDetail} : PostItemType) => {
 
     const { createdAt} = postDetail;
-    
     const { title, body } = JSON.parse(postDetail.title);
     const channelName = postDetail.channel.name;
     const authorName = postDetail.author.fullName;
     const likesNum = postDetail.likes.length; 
     const commentsNum = postDetail.comments.length; 
+
     const timeString = getPostLiveTime();
 
     function getPostLiveTime() {
         const nowTime = new Date();
         const createdTime = new Date(createdAt);
+
         const compareTime = (nowTime.getFullYear() * 525600) -  (createdTime.getFullYear() * 525600) +
                             (nowTime.getMonth() * 43800) -  (createdTime.getMonth() * 43800) + 
                             (nowTime.getDate() * 1440) -  (createdTime.getDate() * 1440) + 
                             (nowTime.getHours() * 60) -  (createdTime.getHours() * 60) + 
                             (nowTime.getMinutes()) -  (createdTime.getMinutes());
-        if (compareTime < 1440)
-            return `${Math.floor(compareTime / 60)}시간전`
-        else 
+        if (compareTime < 60 && compareTime >= 0) {
+            return compareTime === 0 ? '0분 전' : `${Math.floor(compareTime)}분 전`
+        }    
+        else if (compareTime < 1440) {
+            return `${Math.floor(compareTime / 60)}시간 전`
+        }
+        else {
             return `${createdTime.getFullYear().toString().slice(2,4)}.${createdTime.getMonth()+1}.${createdTime.getDate()}`;
+        }
     }
 
     useEffect(()=>{
