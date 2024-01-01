747a306440c33d32dc3422ed79ebdb60e7a0161a
lunarmoon7 <49crehbgr@gmail.com>
2024-01-01 11:35:20 +0900

    feat: 업데이트 된 사용자 정보 저장
    - zustand persist를 사용하여, 업데이트된 사용자 정보를 저장합니다.

--- src/pages/Profile/components/profile-edit-drawer/ProfileNameForm.tsx
+++ src/pages/Profile/components/profile-edit-drawer/ProfileNameForm.tsx
@@ -3,6 +3,7 @@ import tw, { styled } from 'twin.macro'
 import ProfileInput from './ProfileInput'
 import { useProfileStore } from '@/stores/userProfileStore'
 import useForm from '@/hooks/useForm'
+import updateName from '@/apis/updateName'
 
 const Form = styled.form`
   ${tw`w-full flex flex-col`}
@@ -19,22 +20,24 @@ const SubmitButton = styled.button`
 `
 
 function ProfileNameForm() {
-  const { profile } = useProfileStore()
-  const { values, errors, isLoading, handleChange, handleSubmit } = useForm({
+  const { profile, setProfile } = useProfileStore()
+  const { values, isLoading, handleChange, handleSubmit } = useForm({
     initialValue: {
       fullName: profile?.fullName || '',
-      userName: profile?.username || ''
+      username: profile?.username || ''
     },
-    onSubmit: () => {},
-    validate: () => {}
+    onSubmit: async (values) => {
+      const data = await updateName(values)
+      setProfile(data)
+    }
   })
 
   return (
     <Form onSubmit={handleSubmit}>
       <ProfileInput
         labelName="사용자 명"
-        value={values.userName}
-        name="userName"
+        value={values.username}
+        name="username"
         placeholder="사용자 명을 입력해주세요."
         type="text"
         onChangeInput={handleChange}
