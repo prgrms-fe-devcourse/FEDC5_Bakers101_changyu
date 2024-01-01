629f7a9daf893991889f06163de9efc53959d07c
lja0395 <kittyja1023@naver.com>
2024-01-01 09:23:11 +0900

    style: 로그인 스타일 추가

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -1,40 +1,42 @@
-import React, {useState} from 'react';
-import { login } from '@/apis/login'
+import React, { useState } from 'react';
+import { login } from '@/apis/login';
+import * as styles from './LoginStyles';
 
 type Props = {};
 
 function Login({}: Props) {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSumbit = async () => {
-        try {
-          const response = await login(email, password)
-          console.log('response:', response)
-        } catch (error) {
-          console.error(error)
-        }
-      }
-    
-      return (
-        <div>
-          <input
-            type="email"
-            className="border"
-            placeholder="이메일 주소"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            className="border"
-            placeholder="비밀번호"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={handleSumbit}>Login</button>
-        </div>
-      )
+  const handleSumbit = async () => {
+    try {
+      const response = await login(email, password);
+      console.log('response:', response);
+    } catch (error) {
+      console.error(error);
     }
-    
+  };
+
+  return (
+    <styles.container>
+      <styles.form>
+        <styles.input
+          type="email"
+          placeholder="이메일 주소"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <styles.input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <styles.button onClick={handleSumbit}>Login</styles.button>
+        <styles.button>계정이 없으신가요?</styles.button>
+      </styles.form>
+    </styles.container>
+  );
+}
+
 export default Login;
