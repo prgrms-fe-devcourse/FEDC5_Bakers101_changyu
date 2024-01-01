c118269228fb04691c834209fa8684524cf3de12
lunarmoon7 <49crehbgr@gmail.com>
2024-01-01 11:20:42 +0900

    feat: useForm 커스텀 훅 추가
    - 기존 form을 처리하는 로직을 커스텀 훅으로 분리했습니다.

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+type Value = Record<string, string>
+
+interface IUseForm {
+  initialValue: Value
+  onSubmit: (values: Value) => Promise<void>
+  validate?: (values: Value) => any
+}
+
+const useForm = ({ initialValue, onSubmit, validate }: IUseForm) => {
+  const [values, setValues] = useState(initialValue)
+  const [errors, setErrors] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setValues({ ...values, [name]: value })
+    console.log(values)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true)
+    e.preventDefault()
+
+    const newErros = validate ? validate(values) : {}
+    if (Object.keys(newErros).length === 0) {
+      console.log('submit')
+      await onSubmit(values)
+    }
+    setErrors(newErros)
+    setIsLoading(false)
+  }
+
+  return {
+    values,
+    errors,
+    isLoading,
+    handleChange,
+    handleSubmit
+  }
+}
+
+export default useForm
