d25560cc67ca3a6477012404ed973b3b43980caa
JangGwon <tlsakch510@naver.com>
2024-01-04 00:14:41 +0900

    rename : 사용하지 않는 파일 제거했습니다 (postDetail.ts)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import ChannelSelectionModal from './components/channelSelectionsModal';
-import PostList from './components/post/PostList';
+import PostList from './components/Post/PostList';
 import HomeHeader from './components/header';
 import CreatePost from './components/postCreation';
 import HomeBottomNavBar from './components/bottom-navbar';
