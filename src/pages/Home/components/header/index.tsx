bb65fa91fa823bd34882843021ed2737ae60d9b5
JangGwon <tlsakch510@naver.com>
2024-01-04 01:27:43 +0900

    refactor : 메인 홈 헤더 tw적용

--- src/pages/Home/components/header/index.tsx
+++ src/pages/Home/components/header/index.tsx
@@ -1,6 +1,21 @@
+import tw, {styled} from 'twin.macro'
+
 import AlarmIcon from '@/assets/icons/notification.svg'
 import SearchIcon from '@/assets/icons/search.svg'
 import TriangleIcon from '@/assets/icons/triangle.svg'
+
+
+const HomeHeaderContainer = styled.div`
+    ${tw`flex justify-between mx-4`}
+`
+
+const HomeHeaderButtonsWrapper = styled.div`
+    ${tw`flex gap-3 h-fit my-[1.3rem]`}
+`
+
+const ChannelSelectButtonsWrapper = styled.div`
+    ${tw`flex gap-2 mt-3`}
+`
 
 type HomeHeaderProps = {
     selectedChannel : string,
@@ -11,8 +26,8 @@ type HomeHeaderProps = {
 const HomeHeader = ({selectedChannel, onClickChannelList} : HomeHeaderProps) =>{
 
     return (
-        <div className ="flex justify-between mx-4">
-            <div className ="flex gap-2 mt-3">
+        <HomeHeaderContainer>
+            <ChannelSelectButtonsWrapper>
                 <button
                     className = "flex gap-2" 
                     onClick ={onClickChannelList}> 
@@ -21,16 +36,16 @@ const HomeHeader = ({selectedChannel, onClickChannelList} : HomeHeaderProps) =>{
                         className ="h-fit my-[0.6rem] " 
                         src ={TriangleIcon}/>
                 </button>
-            </div>
-            <div className ="flex gap-3 h-fit my-[1.3rem]">
+            </ChannelSelectButtonsWrapper>
+            <HomeHeaderButtonsWrapper>
                 <button>
                     <img src ={SearchIcon}/>
                 </button>
                 <button>
                     <img src ={AlarmIcon}/>
                 </button>
-            </div>
-        </div>
+            </HomeHeaderButtonsWrapper>
+        </HomeHeaderContainer>
     )
 }
 
