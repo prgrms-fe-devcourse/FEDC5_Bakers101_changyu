d253278d64dc95706404571ecdce3e10938589ea
JangGwon <tlsakch510@naver.com>
2024-01-03 22:01:08 +0900

    refactor : 메인 하단 바 이미지들 버튼으로 수정했습니다

--- src/pages/Home/components/bottom-navbar/index.tsx
+++ src/pages/Home/components/bottom-navbar/index.tsx
@@ -12,14 +12,19 @@ const HomeBottomNavBar = ({onClickCreate} : HomeBottomNavBar) =>{
 
     return (
         <div className ='fixed left-1/2 transform -translate-x-1/2 bottom-4 flex gap-3 z-11 bg-white w-48 h-10 justify-center rounded-full drop-shadow-2xl'>
-            <img className = "w-5 h-5 my-auto" src={ProfileIcon}/>
-            <img className = "w-6 h-7 my-auto" src={UserListIcon}/>
-            <img className = "w-6 h-7 my-auto" src={ScrollTopIcon}/>
+            <button>
+                <img className = "w-5 h-5 my-auto" src={ProfileIcon}/>
+            </button>
+            <button>           
+                <img className = "w-6 h-7 my-auto" src={UserListIcon}/>
+            </button>
+            <button>           
+                <img className = "w-6 h-7 my-auto" src={ScrollTopIcon}/>
+            </button>
             <button onClick ={onClickCreate}>
                 <img className = "w-6 h-6 my-auto" src={WriteIcon}/>
             </button>
         </div>
     )
 }
-
 export default HomeBottomNavBar;
