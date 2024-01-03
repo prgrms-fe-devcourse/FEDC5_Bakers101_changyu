0c38ea57199eba290baf98c36b8e24135fb36105
JangGwon <tlsakch510@naver.com>
2024-01-04 00:34:37 +0900

    modify : 메인 홈 하단 바 - 포스트 생성 페이지 이동 버튼 link로 변경했습니다

--- src/pages/Home/components/bottom-navbar/index.tsx
+++ src/pages/Home/components/bottom-navbar/index.tsx
@@ -1,14 +1,12 @@
+import { Link } from 'react-router-dom';
+
 import WriteIcon from '@/assets/icons/write.svg';
 import UserListIcon from '@/assets/icons/userlist.svg';
 import ProfileIcon from '@/assets/icons/profile.svg';
 import ScrollTopIcon from '@/assets/icons/scrollTop.svg';
 
-type HomeBottomNavBar = {
-    onClickCreate : () => void;
-  };
- 
 
-const HomeBottomNavBar = ({onClickCreate} : HomeBottomNavBar) =>{
+const HomeBottomNavBar = () =>{
 
     return (
         <div className ='fixed left-1/2 transform -translate-x-1/2 bottom-4 flex gap-3 z-11 bg-white w-48 h-10 justify-center rounded-full drop-shadow-2xl'>
@@ -21,9 +19,9 @@ const HomeBottomNavBar = ({onClickCreate} : HomeBottomNavBar) =>{
             <button>           
                 <img className = "w-6 h-7 my-auto" src={ScrollTopIcon}/>
             </button>
-            <button onClick ={onClickCreate}>
-                <img className = "w-6 h-6 my-auto" src={WriteIcon}/>
-            </button>
+            <Link to ='/postcreation' className ="h-fit my-auto">
+                <img className = "w-6 h-6" src={WriteIcon}/>
+            </Link>
         </div>
     )
 }
