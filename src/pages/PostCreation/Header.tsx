7c4f4ed95f4213928f4518dd30ecc8bf460315bb
JangGwon <tlsakch510@naver.com>
2024-01-04 02:26:01 +0900

    modify : 포스트 생성 페이지 뒤로가기 버튼 Link 방식으로 변경

--- src/pages/PostCreation/Header.tsx
+++ src/pages/PostCreation/Header.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom'
 import PrevBrownIcon from '@/assets/icons/prev_brown.svg'
 
 const CreatePostHeader = () =>{
     return (
         <div>
-            <button className ="mx-2 my-2 h-[3.125rem]">
+            <Link to="/" className ="mx-2 my-2 h-[3.125rem]">
                 <img src={PrevBrownIcon}/>
-            </button>
+            </Link>
         </div>
     )
 }
