e56ac6502f140acb423aa14a39ac7e96cd3e21df
lunarmoon7 <49crehbgr@gmail.com>
2024-01-04 14:07:14 +0900

    modify: 프로필 불러오는 기능 수정
    - .env를 사용한 하드코딩에서 zustand를 사용하도록 변경

--- src/pages/Profile/components/profile/UserProfileInfo.tsx
+++ src/pages/Profile/components/profile/UserProfileInfo.tsx
@@ -1,6 +1,7 @@
 import tw, { styled } from 'twin.macro'
 import ButtonContainer from './ButtonContainer'
 import FollowButton from './FollowButton'
+import ChatIcon from './ChatIcon'
 
 interface UserProfileInfoProps {
   fullName?: string
@@ -32,8 +33,7 @@ const OnlineIndicator = styled.div(({ isOnline }) => [
 ])
 
 const ChatButton = styled.button`
-  background: url('src/assets/icons/chat.svg') no-repeat center center;
-  ${tw`w-6 h-6`}
+  ${tw`w-5 h-5`}
 `
 
 const UserProfileInfo = ({
@@ -62,7 +62,9 @@ const UserProfileInfo = ({
         </ButtonContainer>
       </ProfileInfo>
       <ButtonContainer className="items-center self-center mb-5">
-        <ChatButton />
+        <ChatButton>
+          <ChatIcon className='w-full h-full'/>
+        </ChatButton>
         {!isMyProfile && isFollowed && (
           <FollowButton
             fill="#FF777F"
