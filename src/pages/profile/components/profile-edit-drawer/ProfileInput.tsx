e9c7363687e368ff84bafe60b8e42959549924a9
lunarmoon7 <49crehbgr@gmail.com>
2024-01-08 11:31:57 +0900

    rename: 폴더 네이밍 컨벤션 적용

---
+++ src/pages/profile/components/profile-edit-drawer/ProfileInput.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import tw, { styled } from 'twin.macro'
+
+interface InputProps {
+  labelName: string
+  name: string
+  value: string
+  placeholder: string
+  type: string
+  onChangeInput: (e: React.ChangeEvent<HTMLInputElement>) => void
+  error?: string
+}
+
+const Input = styled.input`
+  ${tw`w-full border-b outline-none focus:border-brand-primary px-3 py-2`}
+`
+
+const Label = styled.label`
+  ${tw`label`}
+`
+
+const ProfileInput = ({
+  labelName,
+  name,
+  value,
+  placeholder,
+  type,
+  onChangeInput,
+  error
+}: InputProps) => {
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <Label>{labelName}</Label>
+        <p className="text-xs text-[crimson] px-1 py-2 w-fit">{error}</p>
+      </div>
+      <Input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChangeInput}
+      />
+    </div>
+  )
+}
+
+export default ProfileInput
