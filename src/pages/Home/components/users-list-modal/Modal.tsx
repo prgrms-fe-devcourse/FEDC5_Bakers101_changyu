d52de7b0e17a8843b3680f44e671d4bd1de31b9b
lunarmoon7 <49crehbgr@gmail.com>
2024-01-09 13:12:00 +0900

    feat: 사용자 목록 불러오는 기능 연결

--- src/pages/Home/components/users-list-modal/Modal.tsx
+++ src/pages/Home/components/users-list-modal/Modal.tsx
@@ -1,6 +1,9 @@
 import tw, { styled } from 'twin.macro'
 import TabItem from './TabItem'
 import TabContent from './TabContent'
+import { useEffect, useState } from 'react'
+import getAllUsers from '@/apis/user/getAllUsers'
+import getOnlineUsers from '@/apis/user/getOnlineUsers'
 
 interface UsersListProps {
   isOpen: boolean
@@ -21,6 +24,23 @@ const ModalBackdrop = styled.form`
 `
 
 const Modal = ({ isOpen, onToggle }: UsersListProps) => {
+  const [allUsers, setAllUsers] = useState<User[]>([])
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      const data: User[] = await getAllUsers({})
+      setAllUsers(data)
+    }
+
+    const fetchOnlineUsers = async () => {
+      const data: User[] = await getOnlineUsers()
+      setOnlineUsers(data)
+    }
+    fetchAllUsers()
+    fetchOnlineUsers()
+  }, [])
+
   return (
     <ModalContainer
       id="modal"
@@ -31,9 +51,9 @@ const Modal = ({ isOpen, onToggle }: UsersListProps) => {
             title={'모든 사용자'}
             checked
           />
-          <TabContent />
+          <TabContent users={allUsers} />
           <TabItem title={'접속 중인 사용자'} />
-          <TabContent />
+          <TabContent users={onlineUsers} />
         </TabList>
       </div>
       <ModalBackdrop method="dialog">
