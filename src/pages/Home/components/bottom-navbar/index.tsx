085546ade9eac0469b2aba81714a2cd37266bb9f
lunarmoon7 <49crehbgr@gmail.com>
2024-01-09 13:15:28 +0900

    feat: 네비게이션 바에 모달 로직 추가
    - 사용자 목록 아이콘 클릭 시, 모달이 나타납니다.

--- src/pages/Home/components/bottom-navbar/index.tsx
+++ src/pages/Home/components/bottom-navbar/index.tsx
@@ -7,6 +7,8 @@ import UserListIcon from '@/assets/icons/userlist.svg'
 import ProfileIcon from '@/assets/icons/profile.svg'
 import ScrollTopIcon from '@/assets/icons/scrollTop.svg'
 import { useProfileStore } from '@/stores/userProfileStore'
+import { useState } from 'react'
+import Modal from '@/pages/Home/components/users-list-modal/Modal'
 
 const HomeBottomNavBarContainer = styled.div`
   ${tw`fixed left-1/2 transform -translate-x-1/2 bottom-4 flex z-10 bg-white w-56 h-12 justify-evenly rounded-full shadow-lg`}
@@ -14,37 +16,49 @@ const HomeBottomNavBarContainer = styled.div`
 
 const HomeBottomNavBar = () => {
   const { profile } = useProfileStore()
+  const [isUsersListModalOpen, setIsUsersListModalOpen] = useState(false)
+
+  const handleUsersListModal = () => {
+    setIsUsersListModalOpen((prev) => !prev)
+  }
+
   return (
-    <HomeBottomNavBarContainer>
-      <Link
-        to={`/profile/${profile?._id}`}
-        className="h-fit my-auto">
-        <img
-          className="w-5 h-5"
-          src={ProfileIcon}
-        />
-      </Link>
-      <button>
-        <img
-          className="w-7 h-7 my-auto"
-          src={UserListIcon}
-        />
-      </button>
-      <button>
-        <img
-          className="w-7 h-7 my-auto"
-          src={ScrollTopIcon}
-        />
-      </button>
-      <Link
-        to="/post-creation"
-        className="h-fit my-auto">
-        <img
-          className="w-7 h-6"
-          src={WriteIcon}
-        />
-      </Link>
-    </HomeBottomNavBarContainer>
+    <>
+      <HomeBottomNavBarContainer>
+        <Link
+          to={`/profile/${profile?._id}`}
+          className="h-fit my-auto">
+          <img
+            className="w-5 h-5"
+            src={ProfileIcon}
+          />
+        </Link>
+        <button onClick={handleUsersListModal}>
+          <img
+            className="w-7 h-7 my-auto"
+            src={UserListIcon}
+          />
+        </button>
+        <button>
+          <img
+            className="w-7 h-7 my-auto"
+            src={ScrollTopIcon}
+          />
+        </button>
+        <Link
+          to="/post-creation"
+          className="h-fit my-auto">
+          <img
+            className="w-7 h-6"
+            src={WriteIcon}
+          />
+        </Link>
+      </HomeBottomNavBarContainer>
+      <Modal
+        isOpen={isUsersListModalOpen}
+        onToggle={handleUsersListModal}
+      />
+    </>
   )
 }
 export default HomeBottomNavBar
