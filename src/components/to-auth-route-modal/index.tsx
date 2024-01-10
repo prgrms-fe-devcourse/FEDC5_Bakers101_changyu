fc4465121196b1bdb0343738d9a47cc80dc82ef9
JangGwon <tlsakch510@naver.com>
2024-01-11 02:12:11 +0900

    style : 로그인 & 회원가입 이동 안내 모달 디자인

--- src/components/to-auth-route-modal/index.tsx
+++ src/components/to-auth-route-modal/index.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 
 
 const AuthRouteModalContainer = styled.div`
-  ${tw`fixed bg-white`}
+  ${tw`fixed z-10 bg-white border-2 border-slate-300 rounded-md 
+       left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2
+       w-[19rem] py-8`}
 `
 
 interface AuthRouteModal {
@@ -15,14 +17,21 @@ const AuthRouteModal = ({ closeModal} : AuthRouteModal) =>{
   
   return ReactDOM.createPortal(
     <AuthRouteModalContainer>
-      <button onClick ={closeModal}> </button>
-      <p> 아직 Bakers101 회원이 아니신가요?</p>
-      <Link to ='/login' onClick={closeModal}>
-        로그인
-      </Link>
-      <Link to ='/sign-up' onClick={closeModal}>
-        회원가입
-      </Link>
+      <p className='text-center text-lg font-semibold'> 아직 Bakers101 회원이 아니신가요?</p>
+      <div className ="w-fit text-center mx-auto grid gap-4 my-4">
+        <Link className ="px-4 py-1 rounded-md text-white bg-[#9F8170]" to ='/login' onClick={closeModal}>
+          로그인
+        </Link>
+        <Link className ="px-4 py-1 rounded-md text-white bg-[#9F8170]" to ='/sign-up' onClick={closeModal}>
+          회원가입
+        </Link>
+     </div>
+     <div className ="mx-auto w-fit ">
+     <button 
+        onClick ={closeModal}>
+          X
+      </button>
+      </div>
     </AuthRouteModalContainer>,
     document.getElementById('modal-root')as HTMLElement
   )
