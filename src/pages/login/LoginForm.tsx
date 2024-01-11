de64edd4389e54272755036d0b07b0f472593c37
lja0395 <kittyja1023@naver.com>
2024-01-11 19:52:47 +0900

    feat: 로그인 유효성검사 메시지 추가

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -3,20 +3,43 @@ import { useNavigate } from 'react-router-dom'
 import { login } from '@/apis/login'
 import * as Styles from '@/pages/login/LoginStyles'
 import { useProfileStore } from '@/stores/userProfileStore'
+import isPasswordValid from '@/utils/passwordValidator'
 
 function LoginForm() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+  const validateEmail = (input: string): boolean => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    const isValid = emailRegex.test(input)
+    setEmailError(isValid ? '' : '올바른 이메일 주소를 입력하세요.')
+    return isValid
+  }
+
+  const validatePassword = (input: string): boolean => {
+    const isValid = isPasswordValid(input)
+    setPasswordError(
+      isValid ? '' : '최소 8자리 이상, 영문 대소문자 + 숫자, 특수문자 포함'
+    )
+    return isValid
+  }
 
   const { setProfile } = useProfileStore()
   const navigate = useNavigate()
 
   const handleLoginSubmit = async () => {
     try {
-      const response = await login(email, password)
-      localStorage.setItem('token', JSON.stringify(response.token))
-      setProfile(response.user)
-      navigate('/')
+      const isEmailValid = validateEmail(email)
+      const isPasswordValid = validatePassword(password)
+
+      if (isEmailValid && isPasswordValid) {
+        const response = await login(email, password)
+        localStorage.setItem('token', JSON.stringify(response.token))
+        setProfile(response.user)
+        navigate('/')
+      }
     } catch (error) {
       console.log(error)
     }
@@ -25,18 +48,30 @@ function LoginForm() {
   return (
     <Styles.Container>
       <Styles.Form>
-        <Styles.Input
-          type="email"
-          placeholder="이메일 주소"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Styles.Input
-          type="password"
-          placeholder="비밀번호"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <Styles.InputContainer>
+          <Styles.Input
+            type="email"
+            placeholder="이메일 주소"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+              validateEmail(e.target.value)
+            }}
+          />
+          {emailError && <Styles.Error>{emailError}</Styles.Error>}
+        </Styles.InputContainer>
+        <Styles.InputContainer>
+          <Styles.Input
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+              validatePassword(e.target.value)
+            }}
+          />
+          {passwordError && <Styles.Error>{passwordError}</Styles.Error>}
+        </Styles.InputContainer>
         <Styles.Button onClick={handleLoginSubmit}>Login</Styles.Button>
         <Styles.Button onClick={() => navigate('/sign-up')}>
           계정이 없으신가요?
--- src/pages/login/LoginStyles.ts
+++ src/pages/login/LoginStyles.ts
@@ -1,6 +1,8 @@
-import tw from 'twin.macro';
+import tw from 'twin.macro'
 
-export const Container = tw.div`flex items-center justify-center h-screen bg-white`;
-export const Form = tw.div`max-w-4xl w-full p-8`;
-export const Input = tw.input`w-full p-3 bg-transparent border-0 border-b-2 border-gray-300 outline-none focus:border-brand-primary mb-4`;
-export const Button = tw.button`w-full py-2 bg-white border border-gray-300 text-black rounded-full mt-4 hover:bg-brand-primary hover:text-white hover:border-brand-primary transition duration-300 ease-in-out`;
+export const Container = tw.div`flex items-center justify-center h-screen bg-white`
+export const Form = tw.div`max-w-4xl w-full p-8`
+export const InputContainer = tw.div`mb-5 relative`
+export const Input = tw.input`w-full p-3 bg-transparent border-0 border-b-2 border-gray-300 outline-none focus:border-brand-primary mb-6`
+export const Button = tw.button`w-full py-2 bg-white border border-gray-300 text-black rounded-full mt-4 hover:bg-brand-primary hover:text-white hover:border-brand-primary transition duration-300 ease-in-out`
+export const Error = tw.p`text-red-500 text-sm absolute bottom-0 left-0 ml-1`
