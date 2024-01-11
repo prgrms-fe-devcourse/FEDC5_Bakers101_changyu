e94a210efa98d5f810d9c6461f50314217e02050
lja0395 <kittyja1023@naver.com>
2024-01-11 23:39:24 +0900

    feat: 회원가입 후 api 로컬스토리지 저장 및 메인페이지 이동

--- src/pages/sign-up/SignupForm.tsx
+++ src/pages/sign-up/SignupForm.tsx
@@ -1,62 +1,74 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import { signUp } from '@/apis/signup';
-import * as Styles from './SignupStyles';
-import isPasswordValid from '@/utils/passwordValidator';
+import { signUp } from '@/apis/signup'
+import * as Styles from './SignupStyles'
+import isPasswordValid from '@/utils/passwordValidator'
+import { useProfileStore } from '@/stores/userProfileStore'
 
 const SignUpForm = () => {
-
-  const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [nameError, setNameError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const { setProfile } = useProfileStore()
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [nameError, setNameError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+  const [confirmPasswordError, setConfirmPasswordError] = useState('')
 
   const validateEmail = (input: string): boolean => {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const isValid = emailRegex.test(input);
-    setEmailError(isValid ? '' : '올바른 이메일 주소를 입력하세요.');
-    return isValid;
-  };
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    const isValid = emailRegex.test(input)
+    setEmailError(isValid ? '' : '올바른 이메일 주소를 입력하세요.')
+    return isValid
+  }
 
   const validatePassword = (input: string): boolean => {
-    const isValid = isPasswordValid(input);
-    setPasswordError(isValid ? '' : '최소 8자리 이상, 영문 대소문자 + 숫자, 특수문자 포함');
-    return isValid;
-  };
+    const isValid = isPasswordValid(input)
+    setPasswordError(
+      isValid ? '' : '최소 8자리 이상, 영문 대소문자 + 숫자, 특수문자 포함'
+    )
+    return isValid
+  }
 
   const validateName = (input: string): boolean => {
-    const isValid = input.length >= 2;
-    setNameError(isValid ? '' : '이름은 최소 2글자 이상이어야 합니다.');
-    return isValid;
-  };
+    const isValid = input.length <= 6
+    setNameError(isValid ? '' : '이름은 6글자 이내로 입력해주세요.')
+    return isValid
+  }
 
   const validateConfirmPassword = (input: string): boolean => {
-    const isValid = input === password;
-    setConfirmPasswordError(isValid ? '' : '비밀번호와 확인 비밀번호가 일치하지 않습니다.');
-    return isValid;
-  };
+    const isValid = input === password
+    setConfirmPasswordError(
+      isValid ? '' : '비밀번호와 확인 비밀번호가 일치하지 않습니다.'
+    )
+    return isValid
+  }
 
   const handleSignUpSumbit = async () => {
     try {
-      const isEmailValid = validateEmail(email);
-      const isNameValid = validateName(name);
-      const isPasswordValid = validatePassword(password);
-      const isConfirmPasswordValid = validateConfirmPassword(confirmPassword);
+      const isEmailValid = validateEmail(email)
+      const isNameValid = validateName(name)
+      const isPasswordValid = validatePassword(password)
+      const isConfirmPasswordValid = validateConfirmPassword(confirmPassword)
 
-      if (isEmailValid && isNameValid && isPasswordValid && isConfirmPasswordValid) {
-        await signUp(email, name, password);
-        navigate('/login');
+      if (
+        isEmailValid &&
+        isNameValid &&
+        isPasswordValid &&
+        isConfirmPasswordValid
+      ) {
+        const response = await signUp(email, name, password)
+        localStorage.setItem('token', JSON.stringify(response.token))
+        setProfile(response.user)
+        navigate('/')
       }
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   return (
     <Styles.Container>
@@ -68,8 +80,8 @@ const SignUpForm = () => {
             placeholder="이메일 주소"
             value={email}
             onChange={(e) => {
-              setEmail(e.target.value);
-              validateEmail(e.target.value);
+              setEmail(e.target.value)
+              validateEmail(e.target.value)
             }}
           />
           {emailError && <Styles.Error>{emailError}</Styles.Error>}
@@ -81,8 +93,8 @@ const SignUpForm = () => {
             placeholder="이름"
             value={name}
             onChange={(e) => {
-              setName(e.target.value);
-              validateName(e.target.value);
+              setName(e.target.value)
+              validateName(e.target.value)
             }}
           />
           {nameError && <Styles.Error>{nameError}</Styles.Error>}
@@ -94,8 +106,8 @@ const SignUpForm = () => {
             placeholder="비밀번호"
             value={password}
             onChange={(e) => {
-              setPassword(e.target.value);
-              validatePassword(e.target.value);
+              setPassword(e.target.value)
+              validatePassword(e.target.value)
             }}
           />
           {passwordError && <Styles.Error>{passwordError}</Styles.Error>}
@@ -107,16 +119,18 @@ const SignUpForm = () => {
             placeholder="비밀번호 확인"
             value={confirmPassword}
             onChange={(e) => {
-              setConfirmPassword(e.target.value);
-              validateConfirmPassword(e.target.value);
+              setConfirmPassword(e.target.value)
+              validateConfirmPassword(e.target.value)
             }}
           />
-          {confirmPasswordError && <Styles.Error>{confirmPasswordError}</Styles.Error>}
+          {confirmPasswordError && (
+            <Styles.Error>{confirmPasswordError}</Styles.Error>
+          )}
         </Styles.InputContainer>
         <Styles.Button onClick={handleSignUpSumbit}>JOIN</Styles.Button>
       </Styles.Form>
     </Styles.Container>
-  );
+  )
 }
 
-export default SignUpForm;
+export default SignUpForm
