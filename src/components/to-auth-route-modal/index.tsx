a4439d938e78e1d91ea89e8df8a34c6ae3d1ff73
JangGwon <tlsakch510@naver.com>
2024-01-11 09:29:43 +0900

    feat : 로그인 & 회원가입 이동 안내모달 바깥 부분 클릭시 꺼지게하는 기능 구현

--- src/components/to-auth-route-modal/index.tsx
+++ src/components/to-auth-route-modal/index.tsx
@@ -2,6 +2,9 @@ import tw, {styled} from 'twin.macro'
 import { Link } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 
+const ModalOverlay = styled.div`
+  ${tw`fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20`}
+`
 
 const AuthRouteModalContainer = styled.div`
   ${tw`fixed z-10 bg-white border-2 border-slate-300 rounded-md 
@@ -15,24 +18,33 @@ interface AuthRouteModal {
 
 const AuthRouteModal = ({ closeModal} : AuthRouteModal) =>{
   
+  const handleOverlayClick = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) {
+      closeModal()
+    }
+  }
+
   return ReactDOM.createPortal(
+  <ModalOverlay onClick={handleOverlayClick}>
     <AuthRouteModalContainer>
       <p className='text-center text-lg font-semibold'> 아직 Bakers101 회원이 아니신가요?</p>
-      <div className ="w-fit text-center mx-auto grid gap-4 my-4">
-        <Link className ="px-4 py-1 rounded-md text-white bg-[#9F8170]" to ='/login' onClick={closeModal}>
+      <div className="w-fit text-center mx-auto grid gap-4 my-4">
+        <Link className="px-4 py-1 rounded-md text-white bg-[#9F8170]" to ='/login' onClick={closeModal}>
           로그인
         </Link>
-        <Link className ="px-4 py-1 rounded-md text-white bg-[#9F8170]" to ='/sign-up' onClick={closeModal}>
+        <Link className="px-4 py-1 rounded-md text-white bg-[#9F8170]" to ='/sign-up' onClick={closeModal}>
           회원가입
         </Link>
      </div>
-     <div className ="mx-auto w-fit ">
+     <div className="mx-auto w-fit ">
      <button 
-        onClick ={closeModal}>
+        onClick={closeModal}>
           X
       </button>
       </div>
-    </AuthRouteModalContainer>,
+    </AuthRouteModalContainer>
+  </ModalOverlay>
+    ,
     document.getElementById('modal-root')as HTMLElement
   )
 }
