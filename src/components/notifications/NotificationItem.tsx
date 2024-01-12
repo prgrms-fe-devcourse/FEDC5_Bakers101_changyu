016f9fc5e008ca5714da2cf00b49442d91dc0c7f
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-01-12 17:08:55 +0900

    fix: 알림 조건 분기 오류 수정

--- src/components/notifications/NotificationItem.tsx
+++ src/components/notifications/NotificationItem.tsx
@@ -6,16 +6,19 @@ type NotificationItemProps = {
 }
 
 const NotificationItem = memo(({ notification }: NotificationItemProps) => {
-  const { seen, author, post, follow, createdAt } = notification
+  const { seen, author, post, follow, like, createdAt } = notification
 
-  // TODO: 각 데이터들이 어떻게 들어올지 정확한 파악 후 조건문 수정 예정🔨
   const renderNotificationContent = () => {
+    const { fullName } = author
+
+    if (follow) {
+      return `@${fullName}님이 회원님을 팔로우합니다.`
+    }
+
     if (post) {
-      return `@${author.fullName}님이 회원님의 글에 댓글을 남겼습니다.`
-    } else if (follow) {
-      return `@${author.fullName}님이 회원님을 팔로우합니다.`
-    } else {
-      return `@${author.fullName}님이 회원님의 글에 좋아요를 달았습니다.`
+      return like
+        ? `@${fullName}님이 회원님의 글에 좋아요를 달았습니다.`
+        : `@${fullName}님이 회원님의 글에 댓글을 남겼습니다.`
     }
   }
 
