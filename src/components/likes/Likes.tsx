340b508a7fd1bceaf76e0c4361c78fa3ac6762d3
JangGwon <tlsakch510@naver.com>
2024-01-12 16:28:07 +0900

    feat : 로그인 안됐을때 좋아요 버튼 막는 기능 추가

--- src/components/likes/Likes.tsx
+++ src/components/likes/Likes.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useProfileStore } from '@/stores/userProfileStore'
+import { useAuthModalStore } from '@/stores/useAuthModalStore'
 
 import { createLike, deleteLike } from '@/apis/likes'
 import { createNotification } from '@/apis/notifications'
@@ -19,10 +20,14 @@ const LikeButton = ({ postUserId, postId, likeNum }: Props) => {
   const [isLiked, setIsLiked] = useState(false)
   const [likeId, setLikeId] = useState('')
   const { profile, setProfile } = useProfileStore()
+  const { openModal } = useAuthModalStore()
 
   const handleLike = async () => {
     let response = null
     try {
+      if (!profile) {
+        openModal()
+      }
       if (!isLiked) {
         response = await createLike(postId)
         setLikeId(response._id)
