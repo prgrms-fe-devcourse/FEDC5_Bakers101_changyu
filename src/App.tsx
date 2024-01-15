7178b166ec9c204d4537773ea06f714d2a2351a8
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-01-15 11:15:22 +0900

    refactor: Router 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useAuthModalStore } from '@/stores/useAuthModalStore'
-import AuthRouteModal from './components/auth-route-modal'
-
-import Home from './pages/home'
-import PostCreation from './pages/post-creation'
-import PostDetail from './pages/post-detail'
-import SignUpForm from './pages/sign-up/SignupForm'
-import Search from './pages/search'
-import PostEdit from './pages/post-edit'
-import Profile from './pages/profile'
-import LoginForm from './pages/login/LoginForm'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router'
 
 function App() {
-  const {isOpen,closeModal}= useAuthModalStore()
-  
-  return (
-    <BrowserRouter>
-      {isOpen &&  <AuthRouteModal closeModal={closeModal}/>  }
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        <Route
-          path="/login"
-          element={<LoginForm />}
-        />
-        <Route
-          path="/sign-up"
-          element={<SignUpForm />}
-        />
-        <Route
-          path="/post-creation"
-          element={<PostCreation />}
-        />
-        <Route
-          path="/search"
-          element={<Search />}
-        />
-        <Route
-          path="/profile/:id"
-          element={<Profile />}
-        />
-        <Route
-          path="/post-detail/:id"
-          element={<PostDetail />}
-        />
-        <Route
-          path="/post-edit/:id"
-          element={<PostEdit />}
-        />
-        <Route
-          path="/postcreation"
-          element={<PostCreation />}
-        />
-      </Routes>
-    </BrowserRouter>
-
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/routes/router.ts
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Root from './Root'
+
+export const router = createBrowserRouter([{ path: '*', Component: Root }])
