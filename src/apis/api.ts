8f53f76e13d725909c27f02c25f12eb1a0714282
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2024-01-15 11:48:45 +0900

    feat: 토큰이 필요한 요청을 위한 axios 인스턴스 추가

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -6,4 +6,22 @@ const axiosInstance = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 
-export default axiosInstance
+const axiosInstanceWithToken = axios.create({
+  baseURL: `${import.meta.env.VITE_BASE_URL}:${import.meta.env.VITE_APP_PORT}`,
+  timeout: 5000,
+  headers: { 'Content-Type': 'application/json' }
+})
+
+axiosInstanceWithToken.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    const parsedToken = JSON.parse(token as string)
+    config.headers['Authorization'] = `Bearer ${parsedToken}`
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export { axiosInstance, axiosInstanceWithToken }
