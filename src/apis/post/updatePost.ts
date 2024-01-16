9f6984eac28343b8c2295ef298f17bc6a02a0c4a
JangGwon <tlsakch510@naver.com>
2024-01-16 11:14:47 +0900

    fix : 포스트에 썸네일 변경 안되던 오류 수정

--- src/apis/post/updatePost.ts
+++ src/apis/post/updatePost.ts
@@ -3,7 +3,11 @@ import { UPDATE_POST_PATH } from '@/apis/api_paths'
 
 async function updatePost(formData: FormData) {
   try {
-    await axiosInstanceWithToken.put(UPDATE_POST_PATH, formData)
+    await axiosInstanceWithToken.put(UPDATE_POST_PATH, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
   } catch (error) {
     throw new Error(`${error}`)
   }
